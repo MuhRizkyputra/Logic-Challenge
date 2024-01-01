a2716ad4e08336f8cd1ae86593c9828cec9335c7
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-01-01 13:27:17 +0700

    Challange Sum

---
+++ Challange1.js
@@ -0,0 +1,12 @@
+function sum() {
+    let sumAll = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sumAll = sumAll + arguments[i]
+    }
+    console.log(sumAll)
+}
+
+sum(1, 2, 7);
+sum(1, 4);
+sum(11);
+sum(10, 3, 6, 7, 9);
