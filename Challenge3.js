ee6c4177e29a07fd1a55ea338beadba078e3bd93
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-01-08 08:26:30 +0700

    Challenge Romawi

---
+++ Challenge3.js
@@ -0,0 +1,24 @@
+function romawi (n) {
+
+
+const tableRomawi = 
+[
+{awal : 1000 , akhir : 'M'}
+]
+
+
+
+}
+
+
+
+
+
+console.log('Script Testing untuk Konversi Romawi\n');
+console.log('input | expected | result');
+console.log('------|----------|-------');
+console.log('4      |IV         |', romawi(4)); 
+console.log('9      |IX         |', romawi(9));
+console.log('13     |XIII       |', romawi(13));
+console.log('1453   |MCDLIII    |', romawi(1453));
+console.log('16456  |MCDLIII    |', romawi(1646));
